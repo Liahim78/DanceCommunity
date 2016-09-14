3bd3b98a7e7a353c38ff25d2047e78442d966868
Misha <Misha@MISHA->
2016-09-14 13:57:56 +0300

    CommitDB

---
+++ DanceCommunity/DanceCommunity/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DanceCommunity.MvcApplication" Language="C#" %>
---
+++ DanceCommunity/DanceCommunity/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DanceCommunity.Startup))]
+namespace DanceCommunity
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
